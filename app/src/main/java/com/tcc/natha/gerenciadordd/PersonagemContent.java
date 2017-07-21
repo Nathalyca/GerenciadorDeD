80296382414b511f91d15b59473b97499d0b5432
natha <nathalyca03@gmail.com>
2017-07-21 00:47:27 -0300

    PersonagemContent -> Select personagem

--- app/src/main/java/com/tcc/natha/gerenciadordd/PersonagemContent.java
+++ app/src/main/java/com/tcc/natha/gerenciadordd/PersonagemContent.java
@@ -1,12 +1,17 @@
 package com.tcc.natha.gerenciadordd;
 
+import android.support.annotation.NonNull;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import com.tcc.natha.gerenciadordd.models.Personagem;
 
 import java.util.ArrayList;
@@ -14,10 +19,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.tcc.natha.gerenciadordd.models.Personagem;
 
 /**
  * Helper class for providing sample content for user interfaces created by
@@ -27,11 +28,14 @@ import com.tcc.natha.gerenciadordd.models.Personagem;
  */
 public class PersonagemContent {
 
-    private Personagem pers;
+    private static final String TAG = "PersonagemContent";
 
-    private FirebaseUser user;
-    private DatabaseReference mPersReference;
-    private DatabaseReference mDatabase;
+    private static Personagem pers;
+    private static FirebaseUser user;
+    private static FirebaseAuth.AuthStateListener mAuthListener;
+    private static DatabaseReference mDatabase;
+
+    private static ArrayList<String> personagemKeys = new ArrayList();
 
     /**
      * Um array dos personagem para serem visualizados na lista.
@@ -44,48 +48,73 @@ public class PersonagemContent {
     public static final Map<String, PersonagemItem> ITEM_MAP = new HashMap<String, PersonagemItem>();
 
     private static final int COUNT = 25;
-/*
-    pers = new Personagem(user.getUid());
 
-
-    ValueEventListener postListener = new ValueEventListener() {
-        @Override
-        public void onDataChange(DataSnapshot dataSnapshot) {
-            System.out.println("user dataSnapshot.getKey: " + dataSnapshot.getKey());
-            ArrayList<String> personagemKeys = new ArrayList();
-            for (DataSnapshot child: dataSnapshot.getChildren()) {
-                personagemKeys.add(child.getKey());
-            }
-            for (String string: personagemKeys) {
-                System.out.println("personagemKeys: " +string);
-         //       pers.setCarisma(string);
-         //       mCarismaField.setText(pers.getCarisma());
-            }
-        }
-
-        @Override
-        public void onCancelled(DatabaseError databaseError) {
-            System.out.println("The read failed: " + databaseError.getCode());
-        }
-
-    };
-        mDatabase.child("Users").child(user.getUid()).child("personagens").addValueEventListener(postListener);
-
-   */
-//###################
 
     static {
-        // Add some sample items.
-        for (int i = 1; i <= COUNT; i++) {
-            addItem(createPersonagemItem(i));
-        }
+
+        //BD
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                Log.d(TAG, "getCurrentUser");
+
+                user = FirebaseAuth.getInstance().getCurrentUser();
+
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    Log.d(TAG, "onAuthStateChanged:email:" + user.getEmail());
+                } else {
+
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+        FirebaseAuth.getInstance().addAuthStateListener(mAuthListener);
+
+        pers = new Personagem(user.getUid());
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                System.out.println("user dataSnapshot.getKey: " + dataSnapshot.getKey());
+
+                for (DataSnapshot child: dataSnapshot.getChildren()) {
+                    personagemKeys.add(child.getKey());
+                }
+                for (String string: personagemKeys) {
+                    System.out.println("personagemKeys: " +string);
+                    //       pers.setCarisma(string);
+                    //       mCarismaField.setText(pers.getCarisma());
+                }
+                for (int i = 1; i <= COUNT; i++) {
+                    addItem(createPersonagemItem(i));
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+
+        };
+        mDatabase.child("Users").child(user.getUid()).child("personagens").addValueEventListener(postListener);
+
+
     }
 
     private static void addItem(PersonagemItem item) {
         ITEMS.add(item);
         ITEM_MAP.put(item.id, item);
     }
-//###################
+
 
     private static PersonagemItem createPersonagemItem(int position) {
         return new PersonagemItem(String.valueOf(position), "Item " + position, makeDetails(position));
